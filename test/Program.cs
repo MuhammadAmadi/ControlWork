b59200c948ec4f338a4fbf3b61460a878edca3d4
Muhammad <type.92@mail.ru>
2023-03-21 01:46:25 +0300

    END

--- test/Program.cs
+++ test/Program.cs
@@ -17,3 +17,27 @@
     if (length > 0 && arrTemp.Length <= 0) return TestAlgorithm(array, value, length);
     return arrTemp;
 }
+
+string Entry(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine()?? String.Empty;
+}
+
+string[] ArrCreatAndFill(int lenght)
+{
+    string[] arr = new string[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Entry($"Введите любое значение в элемент {i} массива");
+    }
+    return arr;
+}
+
+int lenght = Convert.ToInt32(Entry("Введите длину массива"));
+
+int value = Convert.ToInt32(Entry("Введите длину строки для отбора"));
+
+string[] arr = ArrCreatAndFill(lenght);
+
+Console.Write($"[{string.Join(", ", arr)}] -> [{string.Join(", ",TestAlgorithm(arr, value))}]");
